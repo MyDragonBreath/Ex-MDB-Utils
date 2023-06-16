aba3c23845db9ebf4beef1598570aa16d7b5956f
MyDragonBreath <38029594+MyDragonBreath@users.noreply.github.com>
2023-06-16 18:11:09 +1000

    rc5

--- ExMDBUtils/ExMDBUtils/Properties/AssemblyInfo.cs
+++ ExMDBUtils/ExMDBUtils/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ExMDBUtils")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MyDragonBreath")]
 [assembly: AssemblyProduct("ExMDBUtils")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
